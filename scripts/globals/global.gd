0005ef6e30ee3d4bc31fd82dcc1c8a9fdf2f769c
Marko Žlender <wartney302@gmail.com>
2025-01-20 13:04:08 +0100

    add item icons

--- scripts/globals/global.gd
+++ scripts/globals/global.gd
@@ -6,9 +6,3 @@ extends Node
 @onready var quest_manager: QuestManager
 @onready var utils: Utils
 @onready var inventory: Inventory = $Inventory
-
-func _ready() -> void:
-	inventory.create_and_add_item("sword")
-	inventory.create_and_add_item("potion")
-	
-	print(inventory.get_item_with_prototype_id("potion").get_property("health"))
