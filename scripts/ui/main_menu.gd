6430bfb2523daa82140a4ce2074ae3a125d425eb
Marko Žlender <wartney302@gmail.com>
2025-01-25 15:20:20 +0100

    prevent going to load_screen if there are no save files

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -9,7 +9,10 @@ func _on_new_game_button_pressed() -> void:
 	Global.game_controller.change_gui_scene(ng_slot_selection_scene)
 
 func _on_load_game_button_pressed() -> void:
-	Global.game_controller.change_gui_scene(ld_slot_selection_scene)
+	if Global.save_manager.get_save_files().size() > 0:
+		Global.game_controller.change_gui_scene(ld_slot_selection_scene)
+	else:
+		printerr("Main Menu: _on_load_game_button_pressed: no save files found")
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
