447e8741c5540da5784cf79e1523f291d88196d0
Marko Žlender <wartney302@gmail.com>
2025-01-27 16:07:38 +0100

    open and clsoe inventory

--- scripts/levels/debug_level.gd
+++ scripts/levels/debug_level.gd
@@ -5,4 +5,7 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inventory"):
-		Global.game_controller.change_gui_scene("res://scenes/ui/inventory/inventory_screen.tscn")
+		if !Global.game_controller.gui.has_node("InventoryScreen"):
+			Global.game_controller.change_gui_scene("res://scenes/ui/inventory/inventory_screen.tscn")
+		elif Global.game_controller.gui.has_node("InventoryScreen"):
+			Global.game_controller.change_gui_scene("")		
