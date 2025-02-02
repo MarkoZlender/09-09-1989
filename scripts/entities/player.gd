4a8fe9d7aa2a7bff3d32dd9be31fe409795b74f5
Marko Žlender <wartney302@gmail.com>
2025-02-02 19:58:40 +0100

    cleanup player script

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,122 +1,98 @@
 class_name Player extends Node3D
 const TRAVEL_TIME: float = 0.3
 # Grid settings
-@export var cell_size := Vector3(1, 0, 1)
-@export var move_speed := 5.0
+@export var cell_size: Vector3 = Vector3(1, 0, 1)
+@export var move_speed: float = 5.0
 @export var gridmap: GridMap
+
+enum ROTATION_DIRECTION {
+	LEFT,
+	RIGHT
+}
 
 @onready var camera_rig: Marker3D = %CameraRig
 @onready var animation_player: AnimationPlayer = %AnimationPlayer
 
-var grid_position := Vector3(0.5, 0, 0.5)
+var grid_position: Vector3= Vector3(0.5, 0, 0.5)
 var global_cell_coordinates: Array[Vector3] = []
-var tween
-var is_moving := false
-var inputs = {
+var tween: Tween
+var is_moving: bool= false
+var inputs: Dictionary = {
 	"move_right": Vector3.RIGHT,
 	"move_left": Vector3.LEFT,
 	"move_forward": Vector3.FORWARD,
 	"move_back": Vector3.BACK
 }
 
-func _ready():
+func _ready() -> void:
 	#self.global_transform.origin = gridmap.map_to_local(Vector3i(0, 1, 0))
 	global_position = grid_position
-	for cell in gridmap.get_used_cells():
+	for cell: Vector3i in gridmap.get_used_cells():
 		global_cell_coordinates.append(gridmap.map_to_local(cell))
 
 # func _process(delta: float) -> void:
 # 	handle_movement_input()
 
-func handle_movement_input():
+func handle_movement_input() -> void:
 	if tween is Tween and tween.is_running():
 		return
 	
-	var local_forward = -transform.basis.z
-	var local_back = transform.basis.z
-	var local_left = -transform.basis.x
-	var local_right = transform.basis.x
+	var local_forward: Vector3 = -transform.basis.z
+	var local_back: Vector3 = transform.basis.z
+	var local_left: Vector3 = -transform.basis.x
+	var local_right: Vector3 = transform.basis.x
 
 #region forward_back
+	# not using switch/match because it is slower in gdscript than if-elif-else statements
 	if Input.is_action_pressed("move_forward") && is_position_valid(position + local_forward * cell_size):
-		is_moving = true
 		if !animation_player.is_playing():
 			move(local_forward)
-			animation_player.play("headbob")
-			#play_footsteps()
-
 
 	elif Input.is_action_pressed("move_back") && is_position_valid(position + local_back * cell_size):
 		if !animation_player.is_playing():
-			is_moving = true
 			move(local_back)
-			animation_player.play("headbob")
-			#play_footsteps()
-		
 #endregion
 
 #region strafing
 	elif Input.is_action_pressed("strafe_left") && is_position_valid(position + local_left * cell_size):
 		if !animation_player.is_playing():
-			is_moving = true
 			move(local_left)
-			animation_player.play("headbob")
-			#play_footsteps()
-		
-	
+
 	elif Input.is_action_pressed("strafe_right") && is_position_valid(position + local_right * cell_size):
 		if !animation_player.is_playing():
-			is_moving = true
 			move(local_right)
-			animation_player.play("headbob")
-			#play_footsteps()
-		
 #endregion
 
 #region left_right
 	elif Input.is_action_pressed("move_left") && !Input.is_action_pressed("strafe_left"):
-		is_moving = true
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform", transform.rotated_local(Vector3.UP, PI / 2), TRAVEL_TIME)
-	
+		rotate_player(ROTATION_DIRECTION.LEFT)
+
 	elif Input.is_action_pressed("move_right") && !Input.is_action_pressed("strafe_right"):
-		is_moving = true
-		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
-		tween.tween_property(self, "transform", transform.rotated_local(Vector3.UP, -PI / 2), TRAVEL_TIME)
-	
+		rotate_player(ROTATION_DIRECTION.RIGHT)
+
 	else:
 		is_moving = false
 
 
-func move(direction: Vector3):
+func move(direction: Vector3) -> void:
+	is_moving = true
 	tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "position", position + direction * cell_size, TRAVEL_TIME)
+	animation_player.play("headbob")
+	#play_footsteps()
 
-func attempt_move(dir: String):
-	if is_moving:
-		return
-
-	var new_position = position + inputs[dir]
-	
-	if is_position_valid(new_position):
-		start_movement(new_position)
-
-func start_movement(target_pos: Vector3):
+func rotate_player(direction: ROTATION_DIRECTION) -> void:
 	is_moving = true
+	tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 	
-	tween = create_tween()
-	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
-	tween.tween_property(self, "position", target_pos, 1 / move_speed).set_trans(Tween.TRANS_LINEAR)
-	tween.tween_callback(finalize_movement)
-
-func finalize_movement():
-	is_moving = false
-	# Check if still pressing direction after movement
-	handle_movement_input()
+	if direction == ROTATION_DIRECTION.LEFT:
+		tween.tween_property(self, "transform", transform.rotated_local(Vector3.UP, PI / 2), TRAVEL_TIME)
+	elif direction == ROTATION_DIRECTION.RIGHT:
+		tween.tween_property(self, "transform", transform.rotated_local(Vector3.UP, -PI / 2), TRAVEL_TIME)
 
 
 func is_position_valid(pos: Vector3) -> bool:
-	var cell = gridmap.local_to_map(pos)
+	var cell: Vector3i = gridmap.local_to_map(pos)
 	return gridmap.get_cell_item(cell) != GridMap.INVALID_CELL_ITEM
 
 func save() -> Dictionary:
