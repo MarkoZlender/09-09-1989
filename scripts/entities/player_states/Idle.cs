fdb0dded9fcb40b38dfe811416f06e7d37a4caf3
Marko Žlender <wartney302@gmail.com>
2025-02-28 12:54:49 +0100

    test2

--- scripts/entities/player_states/Idle.cs
+++ scripts/entities/player_states/Idle.cs
@@ -22,6 +22,6 @@ public partial class Idle : Node
     {
         _player.Move(delta);
         if (_player.IsMoving) _stateChart.SendEvent("player_moved");
-        //test
+        //test2
     }
 }
