2bf4bc2690406a05e2856a1f68ede84621739e1e
Marko Žlender <wartney302@gmail.com>
2025-03-02 11:13:47 +0100

    add fallback method for animating input

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -1,30 +1,22 @@
 class_name Player 
 extends CharacterBody3D
 
-const JUMP_VELOCITY = 4.5
-
-@export var SPEED = 5.0
+@export var SPEED: float = 5.0
 @onready var animation_tree: AnimationTree = $AnimationTree
-#@onready var animation: AnimatedSprite3D = $Animation
-var direction
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+var direction: Vector3 = Vector3.ZERO
 var is_moving: bool = false
 var last_facing_direction: Vector2 = Vector2(0, -1)
+var _last_direction: Vector3 = Vector3.ZERO
 
-# func _physics_process(delta: float) -> void:
-# 	move(delta)
 
 func move(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# # Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
-
-	var input_dir := Input.get_vector("left", "right", "up", "down")
-	#animate_input()
+	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down")
 	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
 		is_moving = true
 		velocity.x = direction.x * SPEED
@@ -34,13 +26,41 @@ func move(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
-
 	move_and_slide()
 	animate_input()
 
-func animate_input():
-	var idle = !velocity
-	var blend_position = Vector2(velocity.x, velocity.z).normalized()
+func animate_input() -> void: # fallback method until they fix animation tree errors
+	
+	var input_dir: Vector2 = Input.get_vector("left", "right", "up", "down");
+	direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	if direction != Vector3.ZERO:
+	
+		_last_direction = direction;
+
+		if direction.x > 0:
+			_animation_player.play("run_right");
+		elif direction.x < 0:
+			_animation_player.play("run_left");
+		elif direction.z > 0:
+			_animation_player.play("run_down");
+		elif direction.z < 0:
+			_animation_player.play("run_up");
+	else:
+		if _last_direction.x > 0:
+			_animation_player.play("idle_right");
+		elif _last_direction.x < 0:
+			_animation_player.play("idle_left");
+		elif _last_direction.z > 0:
+			_animation_player.play("idle_down");
+		elif _last_direction.z < 0:
+			_animation_player.play("idle_up");
+		else:
+			_animation_player.play("idle_down");
+
+func animate_input_animation_tree() -> void:
+	var idle: bool = !velocity
+	var blend_position: Vector2 = Vector2(velocity.x, velocity.z).normalized()
 	if !idle:
 		last_facing_direction = blend_position
 
