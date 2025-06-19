06372c8f3d6d298c98d870ae5c7dfdcf34335b40
Marko Žlender <wartney302@gmail.com>
2025-06-19 18:47:22 +0200

    add turn flashlight on and off

--- scripts/utility_scripts/player_model.gd
+++ scripts/utility_scripts/player_model.gd
@@ -6,6 +6,8 @@ extends Node3D
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var is_moving_backwards: bool = false
 @onready var is_hurt:bool = false
+@onready var flashlight: Light3D = %Flashlight
+@onready var flashlight_model: Node3D = %FlashlightModel
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("attack"):
@@ -15,6 +17,11 @@ func _input(event: InputEvent) -> void:
 	elif event.is_action_released("attack"):
 		animation_player.get_animation("attack").loop_mode = Animation.LOOP_NONE
 		attack_state = false
+	
+	if event.is_action_pressed("flashlight"):
+		flashlight.visible = !flashlight.visible
+		flashlight_model.get_node("FlashlightEmission").visible = !flashlight_model.get_node("FlashlightEmission").visible
+		flashlight_model.get_node("FlashlightMesh").mesh.surface_get_material(0).emission_enabled = flashlight.visible
 
 func _physics_process(_delta: float) -> void:
 	velocity = owner.velocity
