08f46e4200c4082d0dee52d6cd319cbf1d3bf281
Marko Žlender <wartney302@gmail.com>
2025-06-21 11:48:22 +0200

    add fixed camera system

---
+++ scenes/environment/fixed_camera.gd
@@ -0,0 +1,21 @@
+extends Camera3D
+
+@onready var area_3d: Area3D = $Area3D
+@onready var audio_listener: AudioListener3D = $AudioListener3D
+
+func _ready() -> void:
+	area_3d.body_entered.connect(_on_body_entered)
+	area_3d.body_exited.connect(_on_body_exited)
+	area_3d.monitorable = false
+	#area_3d.collision_layer = 0
+	#area_3d.collision_mask = 2
+
+func _on_body_entered(body: Node3D) -> void:
+	if body is Player:
+		self.current = true
+		audio_listener.current = true
+
+func _on_body_exited(body: Node3D) -> void:
+	if body is Player:
+		#self.current = false
+		audio_listener.current = false
