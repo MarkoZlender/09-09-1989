63527cdd8a70b4200aef2c689b90a01f33274a73
Marko Žlender <wartney302@gmail.com>
2025-06-23 18:23:18 +0200

    add note interaction sfx

--- scripts/entities/note.gd
+++ scripts/entities/note.gd
@@ -4,6 +4,8 @@ class_name Note extends Node3D
 
 @onready var note_text_label: Label = %NoteTextLabel
 @onready var note_texture: Control = %NoteTexture
+@onready var sfx_player: AudioStreamPlayer3D = $SFXPlayer
+
 
 func _ready() -> void:
 	note_text_label.text = note_text
@@ -11,7 +13,15 @@ func _ready() -> void:
 func _on_interact_area_body_entered(body: Node3D) -> void:
 	if body is Player:
 		note_texture.visible = true
+		sfx_player.play()
 
 func _on_destroy_area_body_exited(body: Node3D) -> void:
 	if body is Player:
-		self.queue_free()
+		_play_and_free()
+
+func _on_sfx_finished() -> void:
+	queue_free()
+
+func _play_and_free() -> void:
+	$SFXPlayer.play()
+	$SFXPlayer.connect("finished", Callable(self, "_on_sfx_finished"))
