64dd2ef34fe9345d3bb5e3371f1fecc9c933ddea
Marko Žlender <wartney302@gmail.com>
2025-06-24 16:27:32 +0200

    add wraith NPC, increase enemy wander range

---
+++ scripts/entities/wraith.gd
@@ -0,0 +1,11 @@
+extends Node3D
+
+@onready var interact_component: InteractComponent = $InteractComponent
+
+func _ready() -> void:
+	interact_component.interact = Callable(self, "_on_interact")
+
+func _on_interact() -> void:
+	var resource: Resource = load("res://assets/dialogue/test_npc_dialogue.dialogue")
+	DialogueManager.show_example_dialogue_balloon(resource)
+	await DialogueManager.dialogue_ended
