e41f402747c7d5ddc6ea129edbe2e7a1fdb06616
Marko Žlender <wartney302@gmail.com>
2025-07-04 21:37:55 +0200

    remove previous camera var from global script

--- scenes/environment/fixed_camera.gd
+++ scenes/environment/fixed_camera.gd
@@ -1,25 +1,3 @@
-# extends Camera3D
-
-# @onready var area_3d: Area3D = $Area3D
-# @onready var audio_listener: AudioListener3D = $AudioListener3D
-
-# func _ready() -> void:
-# 	area_3d.body_entered.connect(_on_body_entered)
-# 	area_3d.body_exited.connect(_on_body_exited)
-# 	area_3d.monitorable = false
-
-# func _on_body_entered(body: Node3D) -> void:
-# 	if body is Player:
-# 		Global.previous_active_camera = get_viewport().get_camera_3d()
-# 		self.current = true
-# 		audio_listener.current = true
-
-
-# func _on_body_exited(body: Node3D) -> void:
-# 	if body is Player:
-# 		Global.previous_active_camera.current = true
-# 		audio_listener.current = false
-
 extends Camera3D
 
 @onready var area_3d: Area3D = $Area3D
