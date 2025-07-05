561ef969d9291ad6ae6e1fce56aeefb34c479bea
Marko Žlender <wartney302@gmail.com>
2025-07-05 12:21:16 +0200

    fix enemies spawning more loot than specified

--- scripts/utility_scripts/collectible_spawner.gd
+++ scripts/utility_scripts/collectible_spawner.gd
@@ -4,9 +4,12 @@ func _ready() -> void:
 	Global.signal_bus.enemy_died.connect(_on_enemy_died)
 
 func _on_enemy_died(enemy: Enemy) -> void:
-	for n: int in enemy.enemy_data.collectible_rigid_body_number:
+	if enemy != owner:
+		return
+	for n: int in range(enemy.enemy_data.collectible_rigid_body_number):
+		print("Spawning collectible: ", n)
 		var collectible: CollectibleRigidBody3D = enemy.enemy_data.collectible_rigid_body.instantiate()
 		get_tree().current_scene.add_child(collectible)
 		collectible.global_position = enemy.global_position
 		# apply force to each collectible so that they are thrown away from the enemy in circle
-		collectible.apply_central_impulse(Vector3(randf_range(-0.2, 0.2), 0.2, randf_range(-0.2, 0.2)) * 10)    
+		collectible.apply_central_impulse(Vector3(randf_range(-0.2, 0.2), 0.2, randf_range(-0.2, 0.2)) * 10)
