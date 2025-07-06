9569286e558d89aebb3254074dea67afc457bb80
Marko Žlender <wartney302@gmail.com>
2025-07-06 16:02:01 +0200

    add news_crime_scene

--- scripts/utility_scripts/open_space.gd
+++ scripts/utility_scripts/open_space.gd
@@ -6,8 +6,10 @@ extends Node3D
 func _ready() -> void:
     if Engine.is_editor_hint():
         world_env.environment.volumetric_fog_enabled = false
+        world_env.environment.glow_enabled = false
     if not Engine.is_editor_hint():
         world_env.environment.volumetric_fog_enabled = true
+        world_env.environment.glow_enabled = true
     
     Global.signal_bus.quest_completed.connect(_on_quest_completed)
     for object: Node3D in get_tree().get_nodes_in_group("removable_objects"):
