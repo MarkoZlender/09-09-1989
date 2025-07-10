d20c19da29e7aa783bff6f051a4f7c117e31b5bc
Marko Žlender <wartney302@gmail.com>
2025-07-10 20:29:47 +0200

    make notes font bigger, remove unnecessary playerdata vars, teset teeth_counter on game restart

--- scripts/data/player_data.gd
+++ scripts/data/player_data.gd
@@ -3,12 +3,8 @@ extends Resource
 
 @export_group("Movement")
 @export var speed: float = 1.5
-@export var rotation_controls: bool = true
 
 @export_group("Combat")
-@export var level: int = 1
-@export var level_progression: Array[int]
-@export var experience: int = 10
 @export var health: int = 100
 @export var max_health: int = 100
 @export var hit_strength: int = 10
