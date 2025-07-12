4f31bdd0f4277463f1b6f208c28692e22e95fafe
Marko Žlender <wartney302@gmail.com>
2025-07-12 10:58:57 +0200

    fix game_over screen

--- scripts/ui/game_over_screen.gd
+++ scripts/ui/game_over_screen.gd
@@ -1,10 +1,11 @@
 extends Control
 
 func _ready() -> void:
+	get_tree().paused = false
 	Global.game_controller.change_3d_scene("")
 
 func _on_restart_button_pressed() -> void:
-	pass
+	Global.game_controller.change_3d_scene(Global.STARTING_LEVEL)
 
 func _on_quit_button_pressed() -> void:
 	Global.game_controller.change_gui_scene(Global.MAIN_MENU_SCENE)
