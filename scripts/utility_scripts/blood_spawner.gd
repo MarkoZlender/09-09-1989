57d4afca4c4d63f4cd8dbc9146e49b2ec21d1d8f
Marko Žlender <wartney302@gmail.com>
2025-07-14 18:22:42 +0200

    fix blood spawning

--- scripts/utility_scripts/blood_spawner.gd
+++ scripts/utility_scripts/blood_spawner.gd
@@ -1,34 +1,58 @@
 extends Node
 
+const BLOOD_TEXTURES: Array = [
+	preload("res://assets/sprites/blood_splatter/tile000.png"),
+	preload("res://assets/sprites/blood_splatter/tile001.png"),
+	preload("res://assets/sprites/blood_splatter/tile002.png"),
+	preload("res://assets/sprites/blood_splatter/tile003.png"),
+	preload("res://assets/sprites/blood_splatter/tile004.png"),
+	preload("res://assets/sprites/blood_splatter/tile005.png"),
+	preload("res://assets/sprites/blood_splatter/tile006.png"),
+	preload("res://assets/sprites/blood_splatter/tile007.png"),
+	preload("res://assets/sprites/blood_splatter/tile008.png"),
+	preload("res://assets/sprites/blood_splatter/tile009.png"),
+	preload("res://assets/sprites/blood_splatter/tile010.png"),
+	preload("res://assets/sprites/blood_splatter/tile011.png"),
+	preload("res://assets/sprites/blood_splatter/tile012.png"),
+	preload("res://assets/sprites/blood_splatter/tile013.png"),
+	preload("res://assets/sprites/blood_splatter/tile014.png"),
+	preload("res://assets/sprites/blood_splatter/tile015.png"),
+	preload("res://assets/sprites/blood_splatter/tile016.png"),
+	preload("res://assets/sprites/blood_splatter/tile017.png"),
+	preload("res://assets/sprites/blood_splatter/tile018.png"),
+	preload("res://assets/sprites/blood_splatter/tile019.png"),
+	preload("res://assets/sprites/blood_splatter/tile020.png"),
+	preload("res://assets/sprites/blood_splatter/tile021.png"),
+	preload("res://assets/sprites/blood_splatter/tile022.png"),
+	preload("res://assets/sprites/blood_splatter/tile023.png"),
+	preload("res://assets/sprites/blood_splatter/tile024.png"),
+	preload("res://assets/sprites/blood_splatter/tile025.png"),
+	preload("res://assets/sprites/blood_splatter/tile026.png"),
+	preload("res://assets/sprites/blood_splatter/tile027.png"),
+	preload("res://assets/sprites/blood_splatter/tile028.png"),
+	preload("res://assets/sprites/blood_splatter/tile029.png"),
+	preload("res://assets/sprites/blood_splatter/tile030.png"),
+	preload("res://assets/sprites/blood_splatter/tile031.png"),
+	preload("res://assets/sprites/blood_splatter/tile032.png"),
+	preload("res://assets/sprites/blood_splatter/tile033.png"),
+	preload("res://assets/sprites/blood_splatter/tile034.png"),
+	preload("res://assets/sprites/blood_splatter/tile035.png"),
+	preload("res://assets/sprites/blood_splatter/tile036.png"),
+	preload("res://assets/sprites/blood_splatter/tile037.png"),
+	preload("res://assets/sprites/blood_splatter/tile038.png"),
+]
+
+
 func _ready() -> void:
 	# Connect the spawn_blood signal to the _spawn_blood_decal method
 	Global.signal_bus.connect(Global.signal_bus.spawn_blood.get_name(), _on_spawn_blood_decal)
 
-func _get_random_blood_texture() -> String:
-	var files: PackedStringArray = DirAccess.get_files_at("res://assets/sprites/blood_splatter/")
-	var blood_textures: PackedStringArray = PackedStringArray()
-	for file: String in files:
-		if file.ends_with(".png"):
-			blood_textures.append(file)
-	if blood_textures.size() == 0:
-		push_error("No blood textures found in the specified directory.")
-		return ""
-	var random_index: int = randi() % blood_textures.size()
-	return blood_textures[random_index]
-
-# func _on_spawn_blood_decal(position: Vector3) -> void:
-# 	if owner.is_hurt:
-# 		return
-# 	else:
-# 		var blood_decal: Decal = Decal.new()
-# 		blood_decal.cull_mask = 1 << 0
-# 		var blood_texture: CompressedTexture2D = load("res://assets/sprites/blood_splatter/" + _get_random_blood_texture())
-# 		if blood_texture:
-# 			blood_decal.texture_albedo = blood_texture
-# 			blood_decal.size = Vector3(1, 1, 1)
-# 			blood_decal.position = position
-# 			blood_decal.rotation = Vector3(owner.rotation.x, randf_range(0, 2 * PI), owner.rotation.z)  # random rotation around Y-axis
-# 			get_tree().get_root().add_child(blood_decal)
+func _get_random_blood_texture() -> Texture2D:
+	if BLOOD_TEXTURES.size() == 0:
+		push_error("No blood textures defined.")
+		return null
+	var random_index: int = randi() % BLOOD_TEXTURES.size()
+	return BLOOD_TEXTURES[random_index]
 
 func _on_spawn_blood_decal(position: Vector3) -> void:
 	if owner is Enemy:
@@ -52,7 +76,7 @@ func _on_spawn_blood_decal(position: Vector3) -> void:
 		var blood_decal: Decal = Decal.new()
 		blood_decal.cull_mask = 1 << 0
 		
-		var blood_texture: CompressedTexture2D = load("res://assets/sprites/blood_splatter/" + _get_random_blood_texture())
+		var blood_texture: CompressedTexture2D = _get_random_blood_texture()
 		if blood_texture:
 			blood_decal.texture_albedo = blood_texture
 			blood_decal.size = Vector3(1, 1, 1)
